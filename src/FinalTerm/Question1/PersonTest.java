e4fd62f0c76ef6746f009825db5c0c99ef630509
wlz0v0 <1228393790@qq.com>
2021-12-15 17:28:49 +0800

    FinalTerm

--- src/FinalTerm/Question1/PersonTest.java
+++ src/FinalTerm/Question1/PersonTest.java
@@ -9,11 +9,7 @@ public class PersonTest {
         var staff = new Staff("Me", "BUPT", "996", "gg@future.good", "Unknown", 0.0, "Unknown");
 
         // 把四个对象保存到Person数组中
-        var persons = new Person[4];
-        persons[0] = person;
-        persons[1] = student;
-        persons[2] = employee;
-        persons[3] = staff;
+        Person[] persons = {person, student, employee, staff};
 
         // 调用所有对象的toString
         for (var p : persons) {
